63a583c10b14061bd4576cbd8a0b9ed49f1ac02f
Ben <tryptefan@gmail.com>
2024-07-10 18:41:55 -0600

    nothing

--- buy_sell.js
+++ buy_sell.js
@@ -637,15 +637,15 @@ refreshLoanValues();
 // @Sam...I'm sure we'll end up putting all this content into objects or something..I just wanted to see these popups with their artowrk
 
 // welcome
-// showMessage(
-//      "it's late april, 1987",
-//      `<p>You've flunked out of college, rent is due, and you're broke. Out of desperation, you decide to try your hand at the
-//      drug game.</p>Against your girlfriend's advice, you hit up notorious loan shark “Big Rick”. He loan's you $5000, at 10%/day interest.</p>
-//      <p>The loan is due in 10 days.<br />Rent is due in 20 days.</p>`,
-//      "Let's Go",
-//      "",
-//      "welcome"
-// );
+showMessage(
+     "it's late april, 1987",
+     `<p>You've flunked out of college, rent is due, and you're broke. Out of desperation, you decide to try your hand at the
+     drug game.</p>Against your girlfriend's advice, you hit up notorious loan shark “Big Rick”. He loan's you $5000, at 10%/day interest.</p>
+     <p>The loan is due in 10 days.<br />Rent is due in 20 days.</p>`,
+     "Let's Go",
+     "",
+     "welcome"
+);
 
 // Jumped
 // showMessage(
@@ -705,13 +705,13 @@ refreshLoanValues();
 // );
 
 // sellSetup
-showMessage(
-     "It's a setup!",
-     "<p>The dude was wearing a wire. Two cops bust into the apartment!</p>",
-     "Flee",
-     "Surrender",
-     "sellSetup"
-);
+// showMessage(
+//      "It's a setup!",
+//      "<p>The dude was wearing a wire. Two cops bust into the apartment!</p>",
+//      "Flee",
+//      "Surrender",
+//      "sellSetup"
+// );
 
 // sellSetupFlee
 // showMessage(
